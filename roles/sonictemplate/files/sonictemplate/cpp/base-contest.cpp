397a05b278991f294a43a00e8cdd81e7423f5156
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2018-02-28 07:45:08 +0900

    Update macros defined in base-contest.cpp
    Use pre-increment.
    
    Change the iteration range of `FOR`, `RFOR`.

--- roles/sonictemplate/files/sonictemplate/cpp/base-contest.cpp
+++ roles/sonictemplate/files/sonictemplate/cpp/base-contest.cpp
@@ -7,10 +7,10 @@
 #include <queue>
 #include <utility>
 #include <vector>
-#define FOR(i,a,b) for (int i=(a);i<(b);i++)
-#define RFOR(i,a,b) for (int i=(b)-1;i>=(a);i--)
+#define FOR(i,a,b) for (int i=(a);i<=(b);++i)
+#define RFOR(i,a,b) for (int i=(a);i>=(b);--i)
 #define REP(i,n) for (int i=0;i<(n);i++)
-#define RREP(i,n) for (int i=(n)-1;i>=0;i--)
+#define RREP(i,n) for (int i=(n)-1;i>=0;--i)
 constexpr int INF = 100100100;
 
 template<typename T>
