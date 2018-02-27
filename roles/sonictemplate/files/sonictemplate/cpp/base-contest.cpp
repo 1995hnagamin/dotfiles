7568a2db935ceecfb30a78fcb145a60c4d2c1733
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2018-02-27 20:52:51 +0900

    Update sonictemplate/cpp/base-contest.cpp
    Add `#include <queue>`.
    Add `constexpr int INF = 100100100;`.
    Define chmin<T>, chmax<T>.

--- roles/sonictemplate/files/sonictemplate/cpp/base-contest.cpp
+++ roles/sonictemplate/files/sonictemplate/cpp/base-contest.cpp
@@ -4,12 +4,24 @@
 #include <iostream>
 #include <map>
 #include <numeric>
+#include <queue>
 #include <utility>
 #include <vector>
 #define FOR(i,a,b) for (int i=(a);i<(b);i++)
 #define RFOR(i,a,b) for (int i=(b)-1;i>=(a);i--)
 #define REP(i,n) for (int i=0;i<(n);i++)
 #define RREP(i,n) for (int i=(n)-1;i>=0;i--)
+constexpr int INF = 100100100;
+
+template<typename T>
+chmin(T &x, T y) {
+  x = std::min(x, y);
+}
+
+template<typename T>
+chmax(T &x, T y) {
+  x = std::max(x, y);
+}
 
 using namespace std;
 
