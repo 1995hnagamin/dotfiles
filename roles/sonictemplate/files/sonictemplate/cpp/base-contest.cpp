fbed48bceab0a4892612424a0099374918177232
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2018-05-03 23:25:41 +0900

    Improve REP macro in base-contest.cpp

--- roles/sonictemplate/files/sonictemplate/cpp/base-contest.cpp
+++ roles/sonictemplate/files/sonictemplate/cpp/base-contest.cpp
@@ -10,7 +10,7 @@
 #include <vector>
 #define FOR(i,a,b) for (int i=(a);i<=(b);++i)
 #define RFOR(i,a,b) for (int i=(a);i>=(b);--i)
-#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP(i,n) for (int i=0, rep_##i##_len=(n);i<(rep_##i##_len);i++)
 #define RREP(i,n) for (int i=(n)-1;i>=0;--i)
 constexpr int INF = 1100100100;
 
